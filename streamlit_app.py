6a5adea1a841df525519fb62a02b4a391afa2ad4
Gov-Github <gbaghele@nicesoftwaresolutions.com>
2024-09-25 16:22:05 +0530

    Update streamlit_app.py
    Added Requests Library

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,3 +58,9 @@ if ingredients_list:
     
     #if ingredients_string:
         #session.sql(my_insert_stmt).collect()
+
+
+#New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
